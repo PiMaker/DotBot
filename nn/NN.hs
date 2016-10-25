c40aa3c93639ffe7529f6fbe73ef5cb4e150608c
Stefan Reiter <Stefan.Reiter@studierende.htl-donaustadt.at>
2016-10-26 00:49:27 +0200

    Finished general NN part

--- nn/NN.hs
+++ nn/NN.hs
@@ -2,49 +2,106 @@ module NN ( think
           , createFullMeshNetwork
           , printNetwork
           , thinkNet
-          , mapNet'
-          , mapConnections
-          , mapInputNeurons
-          , inputId
-          , sigmoid
-          , valueFromConnection
+          , trainNetWithGA
+          , trainNetWithGAFixedCostInput
+          , outputDistance
+          , outputDistanceFixed
           , Connection (..)
-          , Neuron (..) ) where
+          , Neuron (..)
+          , Network) where
 
 import           Data.List
 import qualified Data.Map      as M
-import           System.Random
+import           System.Random.Mersenne.Pure64
+import           Control.Monad
+import qualified Data.ByteString.Lazy.Char8 as B
+import           GHC.Float
 
 data Connection = Connection { weight :: Float
                              , source :: Neuron }
 
-data Neuron = Neuron { nid    :: String
+data Neuron = Neuron { nid    :: B.ByteString
                      , inputs :: [Connection] }
-            | InputNeuron { nid   :: String
+            | InputNeuron { nid   :: B.ByteString
                           , value :: Float }
 
 type Network = [Neuron]
 
+outputDistance :: [Float] -> [Float] -> Network -> Float
+outputDistance i oexp net =
+  let oact = thinkNet net i
+  in sum . map (abs . uncurry (-)) $ zip oexp oact
+
+outputDistanceFixed :: [Float] -> Network -> Float
+outputDistanceFixed oexp net =
+  let oact = map think net
+  in sum . map (abs . uncurry (-)) $ zip oexp oact
+
+allTheSame :: (Eq a) => [a] -> Bool
+allTheSame xs = all (== head xs) (tail xs)
+
+trainNetWithGA :: Network -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Network, PureMT)
+trainNetWithGA net = trainNetWithGAFixedCostInput net []
+
+trainNetWithGAFixedCostInput :: Network -> [Float] -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Network, PureMT)
+trainNetWithGAFixedCostInput net ins cost prob ch iters th rng = do
+  (done, net', rng') <- trainNetWithGA' [] net ins cost prob ch iters th rng
+  if done
+    then return (net', rng')
+    else do
+      putStrLn "Converged early, retrying..."
+      trainNetWithGAFixedCostInput net ins cost prob ch iters th rng'
+
 -- | Trains a network using a genetic algorithm
-trainNetWithGA :: Network -> ([Float] -> Float) -> Float -> Float -> Int -> Int -> StdGen -> Network
-trainNetWithGA net _ _ _ _ 0 _ = net
-trainNetWithGA net cost c prob ch iters rng =
-    --let nets = mapConnections (\c -> c) net
-    trainNetWithGA net cost c prob ch (iters-1) rng
+trainNetWithGA' :: [Float] -> Network -> [Float] -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Bool, Network, PureMT)
+trainNetWithGA' _ net _ _ _ _ 0 _ r = return (True, net, r)
+trainNetWithGA' hist net ins cost prob ch iters th rng =
+        let sameThreshold = iters `div` 4
+        in if length hist > sameThreshold && head hist > th && allTheSame (take sameThreshold hist) then
+              return (False, net, rng)
+            else do -- Prevent early stagnation
+              let prenet = case ins of
+                            [] -> net
+                            _ -> thinkNet' net ins
+              let (net', rng') = genRands prenet prob ch ch rng -- Calculate
+              let net'' = net : net' -- Add original network to prevent reverse progression
+              let costlyNets = map (\x -> (x, cost x)) net''
+              let bestNet = minimumBy (\(_, a) (_, b) -> compare a b) costlyNets
+              when (iters `mod` 100 == 0) (putStrLn ("Iterations left: " ++ show iters ++ " (Current cost: " ++ show (snd bestNet) ++ ")")) -- Status output
+              trainNetWithGA' (snd bestNet : hist) (fst bestNet) ins cost prob ch (iters-1) th rng' -- Recurse
+    where genRands :: Network -> Float -> Int -> Int -> PureMT -> ([Network], PureMT)
+          genRands net _ 0 _ r = ([net], r)
+          genRands net p c cmax r =
+            let (net', r') = mapConnections r (\(con, ri) ->
+                  let (rval', ri') = randomDouble ri
+                      rval = double2Float rval' * 2.0
+                      calc = weight con + (rval-1.0)*p
+                  in (Connection {weight = calc, source = source con}, ri')) net
+                (mnet, r'') = genRands net p (c-1) cmax r'
+            in (net' : mnet, r'')
+
+thinkNet' :: Network -> [Float] -> Network
+thinkNet' net i =
+    let i' = M.fromList $ zip [1..] i
+    in mapInputNeurons (\(InputNeuron nid _) -> InputNeuron {nid = nid, value = M.findWithDefault 0 (inputId nid) i'}) net
 
 thinkNet :: Network -> [Float] -> [Float]
 thinkNet net i =
-    let i' = M.fromList $ zip [0..length i] i
-    in map think $ mapInputNeurons (\(InputNeuron nid _) -> InputNeuron {nid = nid, value = M.findWithDefault 0 (inputId nid) i'}) net
+    map think $ thinkNet' net i
 
-inputId :: String -> Int
-inputId = read . tail
+inputId :: B.ByteString -> Int
+inputId = readInt . B.tail
+
+readInt :: B.ByteString -> Int
+readInt bs = case B.readInt bs of
+                Just (x, _) -> x
+                Nothing -> error "This should never happen (input neuron number could not be parsed)"
 
 mapInputNeurons :: (Neuron -> Neuron) -> Network -> Network
 mapInputNeurons f [] = []
 mapInputNeurons f [Neuron nid inputs] = [Neuron {nid = nid, inputs = map (\(n, o) -> Connection {weight = weight o, source = n}) $ zip (mapInputNeurons f $ map source inputs) inputs}]
 mapInputNeurons f [n] = [f n]
-mapInputNeurons f (n:ns) = mapInputNeurons f [n] ++ mapInputNeurons f ns
+mapInputNeurons f (n:ns) = head (mapInputNeurons f [n]) : mapInputNeurons f ns
 
 mapNet :: (Neuron -> a) -> Network -> [a]
 mapNet f [] = []
@@ -103,7 +160,7 @@ mapConnections s f =
 
 think :: Neuron -> Float
 think (InputNeuron _ value) = value
-think (Neuron _ inputs)     = product $ map (sigmoid . valueFromConnection) inputs
+think (Neuron _ inputs)     = sigmoid . sum $ map valueFromConnection inputs
 
 e = exp 1 :: Float
 
@@ -116,7 +173,7 @@ valueFromConnection (Connection w s) = w * think s
 -- | Input, Hidden, Output
 createFullMeshNetwork :: Int -> [Int] -> Int -> Network
 createFullMeshNetwork inp [] output =
-    let inputNeurons = map ((\x -> InputNeuron {nid = x, value = 0}) . (\x -> "i" ++ show x)) [1..inp]
+    let inputNeurons = map ((\x -> InputNeuron {nid = x, value = 0}) . (\x -> B.pack ("i" ++ show x))) [1..inp]
     in  map (meshNeuron inputNeurons . (\x -> "1" ++ show x)) [1..output]
 createFullMeshNetwork inp [h] output = map m [1..output]
   where m = meshNeuron (createFullMeshNetwork inp [] h)
@@ -126,17 +183,17 @@ createFullMeshNetwork inp (h:hs) output = map m [1..output]
             . (\x -> show (length hs + 2) ++ show x)
 
 meshNeuron :: [Neuron] -> String -> Neuron
-meshNeuron sources name = Neuron {nid = name, inputs = map connectionFromNeuron sources}
+meshNeuron sources name = Neuron {nid = B.pack name, inputs = map connectionFromNeuron sources}
 
 connectionFromNeuron :: Neuron -> Connection
 connectionFromNeuron n = Connection {weight = 1, source = n}
 
 printNetwork :: Network -> String
-printNetwork net = unlines $ mapNet show net
+printNetwork net = unlines $ mapNetOnce show net
 
 instance Show Neuron where
-  show (Neuron n is)     = "Neuron: " ++ n ++ "\n" ++ unlines (map show is)
-  show (InputNeuron n v) = "Input-Neuron: " ++ n ++ " = " ++ show v
+  show (Neuron n is)     = "Neuron: " ++ B.unpack n ++ "\n" ++ unlines (map show is)
+  show (InputNeuron n v) = "Input-Neuron: " ++ B.unpack n ++ " = " ++ show v
 
 instance Show Connection where
-  show connection = "  <---" ++ show (weight connection) ++ "--- " ++ nid (source connection)
+  show connection = "   <---" ++ show (weight connection) ++ "--- " ++ B.unpack (nid (source connection))
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,79 @@
 module Main where
 
 import NN
+import System.Random.Mersenne.Pure64
+import Control.Monad
 
 main :: IO ()
 main = do
+  putStrLn "Commencing NN evaluation..."
   let net = createFullMeshNetwork 2 [2] 1
-  putStrLn $ printNetwork net
+  rng <- newPureMT
+
+  result <- test 500 net rng
+
+  putStrLn result
+
+test :: Int -> Network -> PureMT -> IO String
+test 0 _ _ = return "Done!"
+test i net rng = do
+  -- Train the network
+  (trainedNet, rng') <- trainNetWithGA net (\n -> -- Training data:
+    outputDistance [0.0,0.0] [0.0] n +
+    outputDistance [1.0,0.0] [1.0] n +
+    outputDistance [0.0,1.0] [1.0] n +
+    outputDistance [1.0,1.0] [0.0] n)
+    0.2 -- multiplier
+    80 -- chrildren
+    1000 -- iterations
+    1 -- threshold for early conversion detection
+    rng
+
+  let x1 = round $ head (thinkNet trainedNet [0,0])
+  let x2 = round $ head (thinkNet trainedNet [1,0])
+  let x3 = round $ head (thinkNet trainedNet [0,1])
+  let x4 = round $ head (thinkNet trainedNet [1,1])
+
+  if x1 /= 0 || x2 /= 1 || x3 /= 1 || x4 /= 0 then
+    return $ "Validation failed at: " ++ show rng
+  else do
+    putStrLn ("==== Test complete, remaining: " ++ show i)
+    test (i-1) net rng'
+
+
+
+
+
+-- For demonstrational purposes, not actually used
+main' :: IO ()
+main' = do
+  putStrLn "Beginning simulation..."
+
+  -- Create objects
+  let net = createFullMeshNetwork 2 [2] 1
+  rng <- newPureMT
+
+  -- Train the network
+  (trainedNet, _) <- trainNetWithGA net (\n -> -- Training data:
+    outputDistance [0.0,0.0] [0.0] n +
+    outputDistance [1.0,0.0] [1.0] n +
+    outputDistance [0.0,1.0] [1.0] n +
+    outputDistance [1.0,1.0] [0.0] n)
+    0.2 -- multiplier
+    50 -- chrildren
+    1000 -- iterations
+    1 -- threshold for early conversion detection
+    rng
+
+  -- Test result
+  putStrLn ""
+  putStrLn ("0 XOR 0: " ++ show (round $ head (thinkNet trainedNet [0,0])))
+  putStrLn ("1 XOR 0: " ++ show (round $ head (thinkNet trainedNet [1,0])))
+  putStrLn ("0 XOR 1: " ++ show (round $ head (thinkNet trainedNet [0,1])))
+  putStrLn ("1 XOR 1: " ++ show (round $ head (thinkNet trainedNet [1,1])))
+  putStrLn ""
+
+  -- Print network
+  putStrLn "Network visualization:"
+  putStrLn ""
+  putStrLn $ printNetwork trainedNet
