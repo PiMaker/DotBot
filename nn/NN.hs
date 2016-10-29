1104ddbfa26033f1ba65185e273530119dc625d4
Stefan Reiter <stefan.reiter@studierende.htl-donaustadt.at>
2016-10-29 20:21:04 +0200

    Performance improvements (sigmoid broken

--- nn/NN.hs
+++ nn/NN.hs
@@ -1,11 +1,8 @@
-module NN ( think
-          , createFullMeshNetwork
+module NN ( createFullMeshNetwork
           , printNetwork
           , thinkNet
           , trainNetWithGA
-          , trainNetWithGAFixedCostInput
           , outputDistance
-          , outputDistanceFixed
           , Connection (..)
           , Neuron (..)
           , Network) where
@@ -22,53 +19,43 @@ data Connection = Connection { weight :: Float
 
 data Neuron = Neuron { nid    :: B.ByteString
                      , inputs :: [Connection] }
-            | InputNeuron { nid   :: B.ByteString
-                          , value :: Float }
+            | InputNeuron { nid   :: B.ByteString }
 
 type Network = [Neuron]
+
+type InputMap = M.Map Int Float
 
 outputDistance :: [Float] -> [Float] -> Network -> Float
 outputDistance i oexp net =
   let oact = thinkNet net i
   in sum . map (abs . uncurry (-)) $ zip oexp oact
 
-outputDistanceFixed :: [Float] -> Network -> Float
-outputDistanceFixed oexp net =
-  let oact = map think net
-  in sum . map (abs . uncurry (-)) $ zip oexp oact
-
 allTheSame :: (Eq a) => [a] -> Bool
 allTheSame xs = all (== head xs) (tail xs)
 
 trainNetWithGA :: Network -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Network, PureMT)
-trainNetWithGA net = trainNetWithGAFixedCostInput net []
-
-trainNetWithGAFixedCostInput :: Network -> [Float] -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Network, PureMT)
-trainNetWithGAFixedCostInput net ins cost prob ch iters th rng = do
-  (done, net', rng') <- trainNetWithGA' [] net ins cost prob ch iters th rng
+trainNetWithGA net cost prob ch iters th rng = do
+  (done, net', rng') <- trainNetWithGA' [] net cost prob ch iters th rng
   if done
     then return (net', rng')
     else do
       putStrLn "Converged early, retrying..."
-      trainNetWithGAFixedCostInput net ins cost prob ch iters th rng'
+      trainNetWithGA net cost prob ch iters th rng'
 
 -- | Trains a network using a genetic algorithm
-trainNetWithGA' :: [Float] -> Network -> [Float] -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Bool, Network, PureMT)
-trainNetWithGA' _ net _ _ _ _ 0 _ r = return (True, net, r)
-trainNetWithGA' hist net ins cost prob ch iters th rng =
-        let sameThreshold = iters `div` 4
+trainNetWithGA' :: [Float] -> Network -> (Network -> Float) -> Float -> Int -> Int -> Float -> PureMT -> IO (Bool, Network, PureMT)
+trainNetWithGA' _ net _ _ _ 0 _ r = return (True, net, r)
+trainNetWithGA' hist net cost prob ch iters th rng =
+        let sameThreshold = iters `div` 2
         in if length hist > sameThreshold && head hist > th && allTheSame (take sameThreshold hist) then
               return (False, net, rng)
             else do -- Prevent early stagnation
-              let prenet = case ins of
-                            [] -> net
-                            _ -> thinkNet' net ins
-              let (net', rng') = genRands prenet prob ch ch rng -- Calculate
+              let (net', rng') = genRands net prob ch ch rng -- Calculate
               let net'' = net : net' -- Add original network to prevent reverse progression
               let costlyNets = map (\x -> (x, cost x)) net''
               let bestNet = minimumBy (\(_, a) (_, b) -> compare a b) costlyNets
               when (iters `mod` 100 == 0) (putStrLn ("Iterations left: " ++ show iters ++ " (Current cost: " ++ show (snd bestNet) ++ ")")) -- Status output
-              trainNetWithGA' (snd bestNet : hist) (fst bestNet) ins cost prob ch (iters-1) th rng' -- Recurse
+              trainNetWithGA' (snd bestNet : hist) (fst bestNet) cost prob ch (iters-1) th rng' -- Recurse
     where genRands :: Network -> Float -> Int -> Int -> PureMT -> ([Network], PureMT)
           genRands net _ 0 _ r = ([net], r)
           genRands net p c cmax r =
@@ -80,6 +67,29 @@ trainNetWithGA' hist net ins cost prob ch iters th rng =
                 (mnet, r'') = genRands net p (c-1) cmax r'
             in (net' : mnet, r'')
 
+thinkNet :: Network -> [Float] -> [Float]
+thinkNet net i =
+    let i' = M.fromList $ zip [1..] i
+    in map (think i') net
+
+think :: InputMap -> Neuron -> Float
+think i (InputNeuron n) = M.findWithDefault 0 (readInt $ B.tail n) i
+think i (Neuron _ inputs)     = sigmoid . sum $ map (valueFromConnection i) inputs
+
+e = exp 1 :: Float
+
+sigmoid :: Float -> Float
+sigmoid t = 0.5 * (t / (1 + abs t)) + 0.5 -- TODO ( 1/(1+e**(-t)) )
+
+valueFromConnection :: InputMap -> Connection -> Float
+valueFromConnection m (Connection w s) = w * think m s
+
+readInt :: B.ByteString -> Int
+readInt bs = case B.readInt bs of
+                Just (x, _) -> x
+                Nothing -> error "This should never happen (input neuron number could not be parsed)"
+
+{-
 thinkNet' :: Network -> [Float] -> Network
 thinkNet' net i =
     let i' = M.fromList $ zip [1..] i
@@ -92,16 +102,16 @@ thinkNet net i =
 inputId :: B.ByteString -> Int
 inputId = readInt . B.tail
 
-readInt :: B.ByteString -> Int
-readInt bs = case B.readInt bs of
-                Just (x, _) -> x
-                Nothing -> error "This should never happen (input neuron number could not be parsed)"
-
 mapInputNeurons :: (Neuron -> Neuron) -> Network -> Network
 mapInputNeurons f [] = []
 mapInputNeurons f [Neuron nid inputs] = [Neuron {nid = nid, inputs = map (\(n, o) -> Connection {weight = weight o, source = n}) $ zip (mapInputNeurons f $ map source inputs) inputs}]
 mapInputNeurons f [n] = [f n]
 mapInputNeurons f (n:ns) = head (mapInputNeurons f [n]) : mapInputNeurons f ns
+
+think :: Neuron -> Float
+think (InputNeuron _ value) = value
+think (Neuron _ inputs)     = sigmoid . sum $ map valueFromConnection inputs
+-}
 
 mapNet :: (Neuron -> a) -> Network -> [a]
 mapNet f [] = []
@@ -158,22 +168,10 @@ mapConnections s f =
                   (cs', nxt') = modifyCons nxt f cs
               in (c' : cs', nxt')
 
-think :: Neuron -> Float
-think (InputNeuron _ value) = value
-think (Neuron _ inputs)     = sigmoid . sum $ map valueFromConnection inputs
-
-e = exp 1 :: Float
-
-sigmoid :: Float -> Float
-sigmoid t = 1/(1+(e**(-t)))
-
-valueFromConnection :: Connection -> Float
-valueFromConnection (Connection w s) = w * think s
-
 -- | Input, Hidden, Output
 createFullMeshNetwork :: Int -> [Int] -> Int -> Network
 createFullMeshNetwork inp [] output =
-    let inputNeurons = map ((\x -> InputNeuron {nid = x, value = 0}) . (\x -> B.pack ("i" ++ show x))) [1..inp]
+    let inputNeurons = map ((\x -> InputNeuron {nid = x}) . (\x -> B.pack ("i" ++ show x))) [1..inp]
     in  map (meshNeuron inputNeurons . (\x -> "1" ++ show x)) [1..output]
 createFullMeshNetwork inp [h] output = map m [1..output]
   where m = meshNeuron (createFullMeshNetwork inp [] h)
@@ -193,7 +191,7 @@ printNetwork net = unlines $ mapNetOnce show net
 
 instance Show Neuron where
   show (Neuron n is)     = "Neuron: " ++ B.unpack n ++ "\n" ++ unlines (map show is)
-  show (InputNeuron n v) = "Input-Neuron: " ++ B.unpack n ++ " = " ++ show v
+  show (InputNeuron n) = "Input-Neuron: " ++ B.unpack n
 
 instance Show Connection where
   show connection = "   <---" ++ show (weight connection) ++ "--- " ++ B.unpack (nid (source connection))
--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ import System.Random.Mersenne.Pure64
 
 import Graphics.Gloss
 
+import System.TimeIt
+
 window :: Display
 window = InWindow "Nice Window" (200, 200) (10, 10)
 
@@ -14,21 +16,56 @@ background = white
 drawing :: Picture
 drawing = circle 80
 
+main' :: IO ()
+main' = display window background drawing
+
+
 main :: IO ()
-main = display window background drawing
+main = do
+    demo
+    {-}
+  -- Create objects
+  let net = createFullMeshNetwork 2 [2] 1
+  let rng = pureMT 483209
+
+  avgs <- benchmark net rng [] :: IO [Double]
+
+  putStrLn ""
+  putStrLn $ "Runs: " ++ show (length avgs)
+  putStrLn $ "Total Time: " ++ show (sum avgs)
+  putStrLn $ "Avg. Time: " ++ show (sum avgs / fromIntegral (length avgs))
+  putStrLn ""
+
+benchmark :: Network -> PureMT -> [Double] -> IO [Double]
+benchmark net rng l = do
+  -- Train the network
+  (t, (trainedNet, rng')) <- timeItT $ trainNetWithGA net (\n -> -- Training data:
+    outputDistance [0.0,0.0] [0.0] n +
+    outputDistance [1.0,0.0] [1.0] n +
+    outputDistance [0.0,1.0] [1.0] n +
+    outputDistance [1.0,1.0] [0.0] n)
+    0.2 -- multiplier
+    50 -- chrildren
+    1000 -- iterations
+    1 -- threshold for early conversion detection
+    rng
+
+  if length l < 4 then benchmark net rng' $ t : l else return $ t : l-}
+
+
 
 
 -- For demonstrational purposes, not actually used
-main' :: IO ()
-main' = do
-  putStrLn "Beginning simulation..."
+demo :: IO ()
+demo = do
+  putStrLn "Beginning demonstration..."
 
   -- Create objects
   let net = createFullMeshNetwork 2 [2] 1
   rng <- newPureMT
 
   -- Train the network
-  (trainedNet, _) <- trainNetWithGA net (\n -> -- Training data:
+  (trainedNet, _) <- timeIt $ trainNetWithGA net (\n -> -- Training data:
     outputDistance [0.0,0.0] [0.0] n +
     outputDistance [1.0,0.0] [1.0] n +
     outputDistance [0.0,1.0] [1.0] n +
