262dd495a1b1933960ab14e067bb439b5412920e
Stefan Reiter <Stefan.Reiter@studierende.htl-donaustadt.at>
2016-11-12 18:27:18 +0100

    More web stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import NN
 import System.Random.Mersenne.Pure64
 
-import Graphics.Gloss
+import Data.IORef
 
 import System.TimeIt
 
-window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
-
-background :: Color
-background = white
-
-drawing :: Picture
-drawing = circle 80
-
-main' :: IO ()
-main' = display window background drawing
-
-
 main :: IO ()
 main = do
+    benchMain
+
+benchMain :: IO ()
+benchMain = do
   -- Create objects
   let net = createFullMeshNetwork 2 [2] 1
   let rng = pureMT 483209
@@ -49,40 +42,3 @@ benchmark net rng l = do
     rng
 
   if length l < 9 then benchmark net rng' $ t : l else return $ t : l
-
-
-
-
--- For demonstrational purposes, not actually used
-demo :: IO ()
-demo = do
-  putStrLn "Beginning demonstration..."
-
-  -- Create objects
-  let net = createFullMeshNetwork 2 [2] 1
-  rng <- newPureMT
-
-  -- Train the network
-  (trainedNet, _) <- timeIt $ trainNetWithGA net (\n -> -- Training data:
-    outputDistance [0.0,0.0] [0.0] n +
-    outputDistance [1.0,0.0] [1.0] n +
-    outputDistance [0.0,1.0] [1.0] n +
-    outputDistance [1.0,1.0] [0.0] n)
-    0.2 -- multiplier
-    50 -- chrildren
-    1000 -- iterations
-    1 -- threshold for early conversion detection
-    rng
-
-  -- Test result
-  putStrLn ""
-  putStrLn ("0 XOR 0: " ++ show (round $ head (thinkNet trainedNet [0,0])))
-  putStrLn ("1 XOR 0: " ++ show (round $ head (thinkNet trainedNet [1,0])))
-  putStrLn ("0 XOR 1: " ++ show (round $ head (thinkNet trainedNet [0,1])))
-  putStrLn ("1 XOR 1: " ++ show (round $ head (thinkNet trainedNet [1,1])))
-  putStrLn ""
-
-  -- Print network
-  putStrLn "Network visualization:"
-  putStrLn ""
-  putStrLn $ printNetwork trainedNet
--- web/index.js
+++ web/index.js
@@ -16,15 +16,18 @@ var testModel = {
     }, {
         type: "red",
         x: 2,
-        y: 10
+        y: 10,
+        life: 12
     }, {
         type: "blue",
         x: 4,
-        y: 4
+        y: 4,
+        life: 45
     }, {
         type: "blue",
         x: 6,
-        y: 1
+        y: 1,
+        life: 2
     }, {
         type: "beamRed",
         x1: 2,
@@ -42,6 +45,11 @@ var testModel = {
 
 var model = null;
 var newModel = null;
+
+var blueImg = new Image();
+blueImg.src = "blueImg.png";
+var redImg = new Image();
+redImg.src = "redImg.png";
 
 // Initializes necessary elements for drawing
 function initVis() {
@@ -84,7 +92,22 @@ function drawGrass(model) {
 
 // Draws all elements of a model
 function drawCritters(model) {
-
+    var squareWidth = canvas.width / model.width;
+    var squareHeight = canvas.height / model.height;
+    for (element of model.elements) {
+        if (element.type === "red" || element.type === "blue") {
+            var bitmap = new createjs.Bitmap(element.type === "red" ? redImg : blueImg);
+            bitmap.x = element.x * squareWidth;
+            bitmap.y = element.y * squareHeight;
+            bitmap.scaleX = squareWidth/128;
+            bitmap.scaleY = squareHeight/128;
+            stage.addChild(bitmap);
+            var text = new createjs.Text("" + element.life, "10px Arial", "yellow");
+            text.x = bitmap.x;
+            text.y = bitmap.y;
+            stage.addChild(text);
+        }
+    }
 }
 
 // Draws all beams
